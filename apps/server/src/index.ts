bbcb793138bdea57e8b9faca4696fb50d67f4712
akinloluwami <akinkunmioye42@gmail.com>
2025-12-07 08:22:54 +0100

    initial commit

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -2,14 +2,14 @@ import "dotenv/config";
 import { Elysia } from "elysia";
 import { cors } from "@elysiajs/cors";
 
-const app = new Elysia()
-	.use(
-		cors({
-			origin: process.env.CORS_ORIGIN || "",
-			methods: ["GET", "POST", "OPTIONS"],
-		}),
-	)
-	.get("/", () => "OK")
-	.listen(3000, () => {
-		console.log("Server is running on http://localhost:3000");
-	});
+new Elysia()
+  .use(
+    cors({
+      origin: process.env.CORS_ORIGIN || "",
+      methods: ["GET", "POST", "OPTIONS"],
+    }),
+  )
+  .get("/", () => "OK")
+  .listen(4400, () => {
+    console.log("Server is running on http://localhost:4400");
+  });
